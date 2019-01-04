dcfef55d301b8e1798ad2df94770c442a15fa9fa
wongsitu <wongsitu@ksu.edu>
2019-01-04 14:59:22 -0500

    Heroku

--- product_catalog/settings.py
+++ product_catalog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cqc+$5hv49s(_645u+0byt#xom&qdpqc*2w(v^$)ik5z0$o2pf'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
